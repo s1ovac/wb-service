8ce3c996429be6356479fbf5e44c247cef05a7f5
s1ovac <ilya.pereverzev123@gmail.com>
2022-11-08 19:20:47 +0300

    minor fixes

--- order-subscribe/cmd/main.go
+++ order-subscribe/cmd/main.go
@@ -8,14 +8,15 @@ import (
 	"github.com/s1ovac/order-subscribe/internal/store/config"
 	order "github.com/s1ovac/order-subscribe/internal/store/databases/order/db"
 	"github.com/s1ovac/order-subscribe/internal/store/databases/postgresql"
+	"github.com/s1ovac/order-subscribe/internal/subscribe"
 )
 
 func main() {
-	//sb := subscribe.New()
-	//err := sb.SubscribeToChannel()
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
+	sb := subscribe.New()
+	err := sb.SubscribeToChannel()
+	if err != nil {
+		log.Fatal(err)
+	}
 	cfg := config.NewConfig()
 	postgreSQL, err := postgresql.NewClient(context.TODO(), 3, cfg)
 	if err != nil {
