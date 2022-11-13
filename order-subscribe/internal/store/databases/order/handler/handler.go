716058b3ac3f66d589a93a90e2fb50b6d43f3b6a
Илья Переверзев <s1ovac@DESKTOP-7KHPHVQ.localdomain>
2022-11-13 19:39:12 +0300

    something

--- order-subscribe/internal/store/databases/order/handler/handler.go
+++ order-subscribe/internal/store/databases/order/handler/handler.go
@@ -3,9 +3,11 @@ package handler
 import (
 	"context"
 	"errors"
+	"fmt"
 	"html/template"
 	"net/http"
 
+	"github.com/jackc/pgconn"
 	"github.com/jackc/pgx/v4"
 	"github.com/julienschmidt/httprouter"
 	"github.com/s1ovac/order-subscribe/internal/cache"
@@ -51,6 +53,26 @@ func (h *handler) GetOrderByUUID(w http.ResponseWriter, r *http.Request, params 
 		if err := html.Execute(w, order); err != nil {
 			h.logger.Fatal("Can't execute html view file")
 		}
+		return
+	}
+	var pgErr *pgconn.PgError
+	if errors.As(err, &pgErr) {
+		pgErr = err.(*pgconn.PgError)
+		switch pgErr.Code {
+		case "23505", "22P02":
+			w.WriteHeader(http.StatusNotFound)
+			html, err := template.ParseFiles("/home/s1ovac/github.com/wb-service/order-subscribe/view/notfound.html")
+			if err != nil {
+				h.logger.Fatal("Can't parse html view file")
+			}
+			if err := html.Execute(w, order); err != nil {
+				h.logger.Fatal("Can't execute html view file")
+			}
+			return
+		}
+
+		newErr := fmt.Errorf(fmt.Sprintf("SQL Error: %s, Detail: %s, Where: %s, Code: %s, SQLState: %s", pgErr.Message, pgErr.Detail, pgErr.Where, pgErr.Code, pgErr.SQLState()))
+		h.logger.Error(newErr)
 		return
 	}
 	if err != nil {
